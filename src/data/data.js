5d924add709c5eb0702fcbfcb089c6ee3b6df3a0
AleksandrEnin163 <alexenin163@gmail.com>
2023-09-14 15:15:36 +0200

    add prject

--- src/data/data.js
+++ src/data/data.js
@@ -1,17 +1,18 @@
 import CryptoSearch from '../assets/projects_images/CryptoSearch.png'
 import Qulby from '../assets/projects_images/Qulby.png'
 import GardenShop from '../assets/projects_images/GardenShop.png'
+import FitnessApp from '../assets/projects_images/FitnessApp.png'
 
 export const projects_data = [
-    // {
-    //     id: 1,
-    //     name: 'How to Fitness',
-    //     description: 'Adaptive and responsive ReactApp with more than 1000 fitness exercises splitted to different muscle groups. For each exercise, there is animated gif with instruction, how to do it and 6 explaining videos. There is also a search function, which help to search for the particular exercise, by the name of the exercise, muscle group or equipment.',
-    //     link_git: 'https://github.com/AleksandrEnin163/FitnessApp',
-    //     link_demo: 'https://aleksandrenin163.github.io/FitnessApp/',
-    //     image: ,
-    //     reversed: false
-    // },
+    {
+        id: 1,
+        name: 'How to Fitness',
+        description: 'Adaptive and responsive ReactApp with more than 1000 fitness exercises splitted to different muscle groups. For each exercise, there is animated gif with instruction, how to do it and 6 explaining videos. There is also a search function, which help to search for the particular exercise, by the name of the exercise, muscle group or equipment.',
+        link_git: 'https://github.com/AleksandrEnin163/FitnessApp',
+        link_demo: 'https://aleksandrenin163.github.io/FitnessApp/',
+        image: FitnessApp,
+        reversed: false
+    },
     {
         id: 2,
         name: 'Coin Search',
